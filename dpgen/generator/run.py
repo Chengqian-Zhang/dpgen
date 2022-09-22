15126b554f29204f16a188c7b779c3b79a700d24
root <2000011006@stu.pku.edu.cn>
2022-09-22 16:51:24 +0800

    support merge_traj for lmp template

--- dpgen/generator/run.py
+++ dpgen/generator/run.py
@@ -746,9 +746,12 @@ def revise_lmp_input_model(lmp_lines, task_model_list, trj_freq, deepmd_version 
     return lmp_lines
 
 
-def revise_lmp_input_dump(lmp_lines, trj_freq):
+def revise_lmp_input_dump(lmp_lines, trj_freq, model_devi_merge_traj):
     idx = find_only_one_key(lmp_lines, ['dump', 'dpgen_dump'])
-    lmp_lines[idx] = "dump            dpgen_dump all custom %d traj/*.lammpstrj id type x y z\n" % trj_freq
+    if model_devi_merge_traj: 
+        lmp_lines[idx] = "dump            dpgen_dump all custom %d all.lammpstrj id type x y z\n" % trj_freq
+    else :
+        lmp_lines[idx] = "dump            dpgen_dump all custom %d traj/*.lammpstrj id type x y z\n" % trj_freq
     return lmp_lines
 
 
@@ -1059,7 +1062,7 @@ def _make_model_devi_revmat(iter_index, jdata, mdata, conf_systems):
                 else:
                     lmp_lines = revise_lmp_input_model(lmp_lines, task_model_list, trj_freq, deepmd_version = deepmd_version)
                 
-                lmp_lines = revise_lmp_input_dump(lmp_lines, trj_freq)
+                lmp_lines = revise_lmp_input_dump(lmp_lines, trj_freq, model_devi_merge_traj)
                 lmp_lines = revise_by_keys(
                     lmp_lines, total_rev_keys[:total_num_lmp], total_rev_item[:total_num_lmp]
                 )
